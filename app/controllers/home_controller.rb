0b6440674137552254e7ed6fe1a80d4d1188fa95
Piotr Matusik <piotrmatusik2001@gmail.com>
2017-09-03 16:25:46 +0200

    Add About and Terms page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+  def about
+  end
+  def terms
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get 'fajna_strona', to: 'home#index'
+  get 'about', to: 'home#about'
+  get 'terms', to: 'home#terms'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
