b86409298780647d8a0adb6d7151810000db69df
Piotr Matusik <piotrmatusik2001@gmail.com>
2017-10-17 23:07:25 +0200

    Fix bugs

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
+require "recaptcha/rails"
 Recaptcha.configure do |config|
-  config.public_key  = '6LdLcjQUAAAAABpwlXWhhZKkMMedTMPEvPJ8KSgg'
-  config.private_key = '6LdLcjQUAAAAAJXtwXPwtt9b0UqcY9Hl_oSba6OV'
+  config.site_key  = '6LdLcjQUAAAAABpwlXWhhZKkMMedTMPEvPJ8KSgg'
+  config.secret_key = '6LdLcjQUAAAAAJXtwXPwtt9b0UqcY9Hl_oSba6OV'
 end
